02967e3f96fb40620f03687a6d1b7228df3e5076
gabriel <gillardgabriel4@gmail.com>
2023-10-24 17:36:17 +0200

    login layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.ilang"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ilang"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
