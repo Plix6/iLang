92618ed6b501868a6a66ead804c67a00049c43f9
Plix6 <olivier.bossavit@gmail.com>
2023-10-31 15:58:29 +0100

    Exercise activity data

--- app/src/main/java/com/example/ilang/ExerciseActivity.java
+++ app/src/main/java/com/example/ilang/ExerciseActivity.java
@@ -1,15 +1,51 @@
 package com.example.ilang;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ExerciseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState == null) {
-            // TODO - bind data to buttons
+            ArrayList<Button> answers = new ArrayList<Button>(){
+                {
+                    add((Button) findViewById(R.id.answerOne));
+                    add((Button) findViewById(R.id.answerTwo));
+                    add((Button) findViewById(R.id.answerThree));
+                    add((Button) findViewById(R.id.answerFour));
+                }
+            };
+
+            // TODO - call database API to get data
+
+            ArrayList<String> words = new ArrayList<String>(){
+                {
+                    add("test");
+                    add("another test");
+                    add("incorrect");
+                    add("no");
+                }
+            };
+            String correctWord = "another test";
+            Collections.shuffle(words);
+
+            ArrayList<String> translatedWords = new ArrayList<>();
+            for (String word: words) {
+                // TODO - call translation API to translate guesses
+                translatedWords.add(word);
+                int curIndex = words.indexOf(word);
+                answers.get(curIndex).setText(translatedWords.get(curIndex));
+            }
+
+
+            // TODO - check if correct button is triggered + change colors + save score
         }
         setContentView(R.layout.exercise_layout);
     }
